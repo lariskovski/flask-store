ce8dae2c094d44f7ea9551d2189be6b01dcf33a4
lariskovski <larissaporto@live.com>
2021-09-20 23:53:55 +0000

    Add class method franchise and static method store details

--- store.py
+++ store.py
@@ -4,22 +4,36 @@ class Store:
     def __init__(self, name: str, items=None):
         self.name = name
         self.items = [] if items == None else items
-    
+
+
     def add_item(self, name: str, price: int) -> None:
         item = {"name": name, "price": price}
         self.items.append(item)
+
 
     def stock_price(self) -> int:
         # Add together all item prices in self.items and return the total.
         return sum([item['price'] for item in self.items])
 
 
+    @classmethod
+    def franchise(cls, store):
+        return cls(store.name + ' - franchise')
+
+
+    @staticmethod
+    def store_details(store):
+        return f"{store.name}, total stock price: {store.stock_price()}"
+
 if __name__ == "__main__":
-    flask = Store('Flask Store')
-    flask.add_item('HP Potion', 10)
-    flask.add_item('MP Potion', 10)
-    flask.add_item('Strength Potion', 20)
+
+    flask = Store('Fask Store', [{'name': 'Invulnerability Potion', 'price': 100}])
+    print(Store.store_details(flask))
     print(flask.stock_price())
 
-    big = Store('Big Fask Store', [{'name': 'Invulnerability Potion', 'price': 100}])
-    print(big.stock_price())
+    flask_franchise = Store.franchise(flask)
+    print(flask_franchise.name)
+    flask_franchise.add_item('HP Potion', 10)
+    flask_franchise.add_item('MP Potion', 10)
+    flask_franchise.add_item('Strength Potion', 20)
+    print(flask_franchise.stock_price())
