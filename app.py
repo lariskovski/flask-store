61127a552733047f35da06e51c6d0d570099c5dc
lariskovski <larissaporto@live.com>
2021-09-21 22:29:39 +0000

    Fix route decorator

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 
 
-app =  Flask(__name__)
+app = Flask(__name__)
 
 
 stores = [{
@@ -15,27 +15,29 @@ stores = [{
 
 
 # POST /store data: {name :}
-app.route('/store', methods=['POST'])
+@app.route('/store', methods=['POST'])
 def create_store():
     pass
 
 # GET /store/<name> data: {name :}
-app.route('/store/<string:name>')
+@app.route('/store/<string:name>')
 def get_store(name):
     pass
 
 # GET /store
-app.route('/store')
+@app.route('/')
 def get_stores():
-    return jsonify({'stores': stores})
+    return jsonify({
+                    'stores': stores
+                   })
 
 # POST /store/<name>/item data: {name :}
-app.route('/store/<string:name>/item', methods=['POST'])
+@app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
     pass
 
 # GET /store/<name>/item data: {name :}
-app.route('/store/<string:name>/item')
+@app.route('/store/<string:name>/item')
 def get_items_in_store(name):
     pass
 
