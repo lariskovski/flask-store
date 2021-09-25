cc4d676e61736336c570e174409dbac518b5f679
Larissa Porto <larissaporto@live.com>
2021-09-25 18:19:27 -0300

    Improve response time greatly

--- src/app.py
+++ src/app.py
@@ -7,6 +7,11 @@ from flask import Flask
 from resources.item import Item, ItemList
 from resources.user import UserRegister
 
+from gevent.pywsgi import WSGIServer
+from gevent import monkey
+
+monkey.patch_all()
+
 app = Flask(__name__)
 app.secret_key = 'onetwothree'
 api = Api(app)
@@ -22,4 +27,10 @@ api.add_resource(UserRegister, '/register')
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
+    # app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
+
+    # use gevent WSGI server instead of the Flask
+    http = WSGIServer(('', 5000), app.wsgi_app)
+
+    # Serve your application
+    http.serve_forever()
