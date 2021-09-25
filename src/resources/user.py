779a3469a7589c6ca7aae06b74ba150086d43a1f
Larissa Porto <larissaporto@live.com>
2021-09-25 10:17:35 -0300

    create user model package and fix imports

--- src/resources/user.py
+++ src/resources/user.py
@@ -1,46 +1,7 @@
 import sqlite3
 from flask_restful import Resource, reqparse
 
-
-class User:
-    def __init__(self, _id, username, password) -> None:
-        self.id = _id
-        self.username = username
-        self.password = password
-    
-    @classmethod
-    def find_by_username(cls, username):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query, (username,))
-        row = result.fetchone()
-
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-        
-        connection.close()
-        return user
-    
-    @classmethod
-    def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query, (_id,))
-        row = result.fetchone()
-
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-        
-        connection.close()
-        return user
+from models.user import UserModel
 
 
 class UserRegister(Resource):
@@ -63,8 +24,8 @@ class UserRegister(Resource):
         data = parser.parse_args()
 
         # Makes sure usernames are unique
-        if User.find_by_username(data['username']):
-            return {"message": f"User {data['username']} already exists"}, 400
+        if UserModel.find_by_username(data['username']):
+            return {"message": f"UserModel {data['username']} already exists"}, 400
 
         # Create connection
         conn = sqlite3.connect('data.db')
@@ -78,4 +39,4 @@ class UserRegister(Resource):
         conn.commit()
         conn.close()
 
-        return {"message": "User created successfully." }, 201
+        return {"message": "UserModel created successfully." }, 201
