96c001ab2a969eb130c9e2fc602b5b54634da416
Larissa Porto <larissaporto@live.com>
2021-09-27 20:45:20 -0300

    Remove unused import from user resource

--- src/resources/user.py
+++ src/resources/user.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
-from flask_jwt_extended import create_access_token, create_refresh_token, get_current_user
+from flask_jwt_extended import create_access_token, create_refresh_token
 from models.user import UserModel
 
 
