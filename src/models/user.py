0ae5956a3a713ae0ed242dfc5cae4c524f735186
Larissa Porto <larissaporto@live.com>
2021-09-27 20:43:50 -0300

    Implement flask jwt extended auth

--- src/models/user.py
+++ src/models/user.py
@@ -21,6 +21,17 @@ class UserModel(db.Model):
     def find_by_id(cls, _id):
         return cls.query.filter_by(id=_id).first()
 
+    @classmethod
+    def get_all(cls):
+        return cls.query.all()
+
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def json(self):
+        return {"id": self.id, "username": self.username}
--- src/resources/user.py
+++ src/resources/user.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse
-
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import create_access_token, create_refresh_token, get_current_user
 from models.user import UserModel
 
 
@@ -30,3 +31,56 @@ class UserRegister(Resource):
         user.save_to_db()
 
         return {"message": "User created successfully"}, 201
+
+
+class User(Resource):
+    @classmethod
+    def get(clas, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'user not found'}, 404
+        return user.json()
+
+    @classmethod
+    def delete(clas, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'user not found'}, 404
+        user.delete_from_db()
+        return {"message": 'user deleted'}, 200
+
+
+class UserList(Resource):
+    def get(self):
+        return {"item": [item.json() for item in UserModel.get_all()]}
+
+
+class UserLogin(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'username',
+        type=str,
+        required=True,
+        help="This filed cannot be left blank"
+    )
+    parser.add_argument(
+        'password',
+        type=str,
+        required=True,
+        help="This filed cannot be left blank"
+    )
+
+    @classmethod
+    def post(cls):
+        data = cls.parser.parse_args()
+
+        user = UserModel.find_by_username(data['username'])
+        if user and safe_str_cmp(user.password, data['password']):
+            access_token = create_access_token(identity=user.id)
+            refresh_token = create_refresh_token(user.id)
+            return {
+                'access_token': access_token,
+                'refresh_token': refresh_token
+            }, 200
+
+        return {'message': 'invalid credentials'}, 401
